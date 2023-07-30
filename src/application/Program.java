0a4f09b8da296006ef7b549e94e070bd6b403865
wandeson <wandesonjsantos7@gmail.com>
2023-07-30 08:15:59 -0300

    Moving pieces, handling exceptions and clearing screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,44 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import chess.ChessException;
 import chess.ChessMacth;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
-
+		
+		Scanner sc = new Scanner (System.in);
 		ChessMacth chessMacth = new ChessMacth();
-		UI.printBoard(chessMacth.getPieces());
+	
+		while (true) {
+			try {
+				UI.clearScrenn();
+				UI.printBoard(chessMacth.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+			
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+
+			
+			ChessPiece capturedPiece = chessMacth.performChessMove(source, target);
+		}
+			catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}	
+		}
 	}
 
 }
